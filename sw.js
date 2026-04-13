7ab65ba49275ce21a7db8e66ab1872a63da20b22
Duam Rodrigues <3drxdzin@gmail.com>
2026-04-13 20:36:37 -0300

    feat: sidebar reestruturado em grupos semanticos (Visão Geral, Controle, Segurança, rodapé fixo)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cache-first para o shell da aplicação
 // Ao fazer deploy: incrementar CACHE_NAME para invalidar cache antigo
 
-const CACHE_NAME = 'dmsmart-v04132355';
+const CACHE_NAME = 'dmsmart-v04131010';
 
 const SHELL_ASSETS = [
   '/',
